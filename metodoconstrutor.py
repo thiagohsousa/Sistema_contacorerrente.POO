2372046411e50b50d254da41b17e5d994a7d595f
Thiago <thiagohs.dev@gmail.com>
2025-12-29 19:25:48 -0300

    Adição de um sistema de transferir e organização das informações

--- metodoconstrutor.py
+++ metodoconstrutor.py
@@ -4,4 +4,31 @@ class Conta_corrente():
         self.Saldo = Saldo
         self.Cpf = Cpf
         self.idade = Idade
+    def transferir(self):
+        transferencia = float(input("Digite o quanto vc deseja transferir: "))
+        if self.Saldo >= transferencia:
+            self.Novo_saldo = self.Saldo - transferencia
+            print(F"""-------------------------------
+TRANSFERENCIA CONCLUIDA 
+-------------------------------
+                  
+SALDO DISPONIVEL: {self.Novo_saldo}
+-------------------------------""")
+            
+
+    def informacoes(self):
+        print(f"""------------------------------- 
+INFORMAÇÕES
+-------------------------------
+          
+1 - Titular: {self.Titular}
+
+
+2 - Cpf: {self.Cpf}
+
+3 - Idade: {self.idade}
+
+4 - Saldo Disponivel: {self.Novo_saldo}""")
+  
+
 
