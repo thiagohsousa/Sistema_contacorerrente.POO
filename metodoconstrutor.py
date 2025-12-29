aaccd7e7ba683afa54452f65c3f72fb96ba84343
Thiago <thiagohs.dev@gmail.com>
2025-12-29 18:47:34 -0300

    Adicionando função de tranferir e ajustando o menu de informações

--- metodoconstrutor.py
+++ metodoconstrutor.py
@@ -4,4 +4,29 @@ class Conta_corrente():
         self.Saldo = Saldo
         self.Cpf = Cpf
         self.idade = Idade
+  
+    def transferir(self):
+            transferir = float(input("Digite o   Quanto que voce deseja tranferir: "))
+            if self.Saldo >= transferir:
+                self.Novo_saldo = self.Saldo - transferir
+                print(f"""Tranferencia Concluida
+                      
+                      Saldo Atual: {self.Novo_saldo}""")
+                
+    def informacoes(self):
+         print(f""" -------------------------------------- 
+      Informações
+--------------------------------------     
+      
+1- Titular: {self.Titular}\n
+2- Idade: {self.idade}\n
+3- Cpf: {self.Cpf}\n
+4- Saldo: {self.Novo_saldo}\n
+
+--------------------------------------  """)
+
+         
+
+
+        
 
--- metodomodularizado.py
+++ metodomodularizado.py
@@ -1,18 +1,11 @@
 from metodoconstrutor import Conta_corrente
 
 
+
 clienteum = Conta_corrente(Titular='Thiago Henrique Sousa Melo', Idade=16, Cpf="011.111.111-11", Saldo=150000.0)
 
 
+clienteum.transferir()
+clienteum.informacoes()
 
 
-print(f""" -------------------------------------- 
-      Informações
---------------------------------------     
-      
-1- Titular: {clienteum.Titular}\n
-2- Idade: {clienteum.idade}\n
-3- Cpf: {clienteum.Cpf}\n
-4- Saldo: {clienteum.Saldo}\n
-
---------------------------------------  """)
