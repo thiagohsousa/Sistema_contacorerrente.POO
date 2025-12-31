982040959cfe09f1d5210abe82d324044010db20
Thiago <thiagohs.dev@gmail.com>
2025-12-31 17:43:18 -0300

    Criando um gerador de ID e corrigindo Bugs

--- metodoconstrutor.py
+++ metodoconstrutor.py
@@ -1,36 +1,47 @@
+from random import randint
+
 #Classe para criar uma conta corrente
 class Conta_corrente():
-    def __init__(self,Titular,Idade, Cpf,  Saldo):
-        self._titular = Titular      
-        self._idade = Idade
-        self._cpf = Cpf
-        self._Saldo = Saldo
+      def __init__(self, titular="", idade=0, cpf="", saldo=0):
+         while True:
+            try:
+                self._titular = input("Digite o nome do titular: ")
+                self._idade =  int(input("Digite a idade do titular: "))
+                self._cpf = input("Digite o CPF do titular: ")
+                self._saldo =  float(input("Digite o saldo inicial: "))
+                self._conta_id = self.gerar_id()
+                break
+            except:
+               print("Idade ou Cpf Invalidos, Tente Novamente!!")
+
+    
+
 
 
    #METODO GETTER: Le o valor
-    @property
-    def saldo(self):
-        return self._Saldo
+      @property
+      def saldo(self):
+            return self._saldo
 
     #Metodo SETTER: Altera o Valor e trata os erros
-    @saldo.setter
-    def novo_saldo(self, novo_saldo):
+      @saldo.setter
+      def novo_saldo(self, novo_saldo):
         if isinstance(novo_saldo, (int, float)) and novo_saldo >= 0:
-            self._Saldo = novo_saldo
+            self._saldo = novo_saldo
         else:
             print("Saldo Invalido!!!")
 
     #logica sem loop para transferir, incluirei na função transferir.
-    def funcao_transferir(self):
+      def funcao_transferir(self):
       
         transferencia = float(input("Digite o quanto vc deseja transferir: "))
-        if self._Saldo >= transferencia:
-            self._Saldo -= transferencia
+        if self._saldo >= transferencia:
+            self._saldo -= transferencia
             print(F"""-------------------------------
 TRANSFERENCIA CONCLUIDA 
 -------------------------------
 
-SALDO DISPONIVEL: {self._Saldo}
+SALDO DISPONIVEL: {self._saldo}
 -------------------------------""")
         
             
@@ -38,7 +49,7 @@ SALDO DISPONIVEL: {self._Saldo}
             print("Saldo Insuficiente, Tente Novamente")
 
      #função para transferir  com o loop.
-    def tranferencia(self):
+      def tranferencia(self):
         while True:
             self.funcao_transferir()
             continuar = input("Deseja Continuar? [S/N]: ")
@@ -48,27 +59,27 @@ SALDO DISPONIVEL: {self._Saldo}
 
 
    #METODO GETTER: Le o valor
-    @property
-    def sacar(self):
-        return self._Saldo
+      @property
+      def sacar(self):
+        return self._saldo
     #Metodo SETTER: Altera o Valor e trata os erros
-    @sacar.setter
-    def saldo_saque(self, sacar):
+      @sacar.setter
+      def saldo_saque(self, sacar):
         if isinstance(sacar, (int, float)) and sacar >= 0:
-            self._Saldo = sacar
+            self._saldo = sacar
     #Logica sem Lopp para sacar, incluirei ela na função Saque
-    def funcao_saque(self):
+      def funcao_saque(self):
         sacar = float(input("Digite quanto vc deseja sacar: "))
-        if sacar <= self._Saldo:
-            self._Saldo -= sacar
+        if sacar <= self._saldo:
+            self._saldo -= sacar
             print(f"""-------------------------------
 Saque Realizado
-Seu Novo saldo é de {self._Saldo}
+Seu Novo saldo é de {self._saldo}
 -------------------------------""")
         else:
             print("Saque invalido!!!")
     #Funçaõ para sacar com loop
-    def saque(self):
+      def saque(self):
         while True:
             self.funcao_saque()
             continuar =  continuar = input("Deseja Continuar? [S/N]: ")
@@ -84,7 +95,7 @@ Seu Novo saldo é de {self._Saldo}
 
 
 #Função que Printa todas as informações da conta
-    def informacoes(self):
+      def informacoes(self):
         print(f"""------------------------------- 
 INFORMAÇÕES
 -------------------------------
@@ -96,7 +107,15 @@ INFORMAÇÕES
 
 3 - Idade: {self._idade}
 
-4 - Saldo Disponivel: {self._Saldo}""")
-  
-            
+4- Id da Conta: {self._conta_id}
 
+5 - Saldo Disponivel: {self._saldo}""")
+  
+
+
+
+    
+    
+
+      def gerar_id(self):
+        return randint(1, 1000)
--- metodomodularizado.py
+++ metodomodularizado.py
@@ -3,28 +3,23 @@ from metodoconstrutor import Conta_corrente
 
 
 # Criar conta 
-while True:
-    try:
-        titular = input("Digite o nome do titular: ")
-        idade = int(input("Digite a idade do titular: "))
-        cpf = input("Digite o CPF do titular: ")
-        saldo = float(input("Digite o saldo inicial: "))
-        break
-    except ValueError:
-        print("Valores inválidos, tente novamente")
+
+
 
 # Criando a conta
-clienteum = Conta_corrente(Titular=titular, Idade=idade, Cpf=cpf, Saldo=saldo)
-print(f"Conta de {clienteum._titular} foi criada com sucesso!\n")
+
+
+conta = Conta_corrente()
+print(f"Conta de {conta._titular} foi criada com sucesso!\n")
 
 # Método para transferir
-clienteum.tranferencia()
+conta.tranferencia()
 
 # Método para mostrar informações
-clienteum.informacoes()
+conta.informacoes()
 
 #Método para Sacar
-clienteum.saque()
+conta.saque()
 
 
 
